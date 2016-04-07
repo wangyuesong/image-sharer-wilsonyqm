46d846934cea9255e6b8d172e4d7e68bf8d21e23
Qiaomu Yao <wilsonyqm@gmail.com>
2016-04-07 16:00:00 -0700

    qy-display all images on index page
    - Constrain images to be 400px wide max

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -1,10 +1,19 @@
 require 'test_helper'
 
 class ImagesControllerTest < ActionController::TestCase
-  test 'index' do
+  test 'index with images' do
+    image_url_1 = 'http://www.horniman.info/DKNSARC/SD04/IMAGES/D4P1570C.JPG'
+    image_url_2 = 'http://images.all-free-download.com/images/graphiclarge/page_90297.jpg'
+    Image.create!(title: 'test1Img', url: image_url_1)
+    Image.create!(title: 'test2Img', url: image_url_2)
+
     get :index
     assert_response :success
-    assert_select '#hello_world', 1
+    assert_select 'tr', count: 3
+    assert_select 'img', count: 2
+    assert_select "img[src=\"#{image_url_1}\"]", 1
+    assert_select "img[src=\"#{image_url_2}\"]", 1
+
   end
 
   test 'new' do
@@ -22,11 +31,11 @@ class ImagesControllerTest < ActionController::TestCase
 
   test 'show' do
     image_url = 'http://www.horniman.info/DKNSARC/SD04/IMAGES/D4P1570C.JPG'
-    image = Image.create!(title: 'test1Img', url: image_url)
+    image = Image.create!(title: 'test3Img', url: image_url)
 
     get :show, id: image
     assert_response :success
     assert_select "img[src=\"#{image_url}\"]", 1
-    assert_select '#image_title', text: 'test1Img'
+    assert_select '#image_title', text: 'test3Img'
   end
 end
