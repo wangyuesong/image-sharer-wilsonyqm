e28ac50bc3c9bc3a5094affd5bb5927d69643230
Qiaomu Yao <wilsonyqm@gmail.com>
2016-04-14 15:58:15 -0700

    qy - Add some styling to the images index page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def nav_item(text:, path:, is_active:)
+    classes = ['nav-item']
+    classes << 'active' if is_active
+    content_tag :li, class: classes do
+      link_to text, path, class: 'nav-link'
+    end
+  end
 end
