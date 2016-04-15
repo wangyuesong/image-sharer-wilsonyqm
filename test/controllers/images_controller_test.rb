dd52855296eb79e952b678a4db4a9fccde6d4785
Qiaomu Yao <wilsonyqm@gmail.com>
2016-04-14 17:15:06 -0700

    qy - add tags to images

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -2,18 +2,21 @@ require 'test_helper'
 
 class ImagesControllerTest < ActionController::TestCase
   test 'index with images' do
-    image_url_1 = 'http://www.horniman.info/DKNSARC/SD04/IMAGES/D4P1570C.JPG'
-    image_url_2 = 'http://images.all-free-download.com/images/graphiclarge/page_90297.jpg'
-    Image.create!(title: 'test1Img', url: image_url_1)
-    Image.create!(title: 'test2Img', url: image_url_2)
+    url1 = 'http://www.horniman.info/DKNSARC/SD04/IMAGES/D4P1570C.JPG'
+    url2 = 'http://images.all-free-download.com/images/graphiclarge/page_90297.jpg'
+    image_attributes = [
+      {title: 'image1', url: url1, tag_list: 'tag1, tag2'},
+      {title: 'image2', url: url2, tag_list: 'tag3, tag4'}
+    ]
+    images = image_attributes.map { |attributes| Image.create!(attributes) }
 
     get :index
     assert_response :success
-    assert_select 'tr', count: 3
-    assert_select 'img', count: 2
-    assert_select "img[src=\"#{image_url_1}\"]", 1
-    assert_select "img[src=\"#{image_url_2}\"]", 1
 
+    images.each do |image|
+      assert_select "tr[data-image-id=\"#{image.id}\"] img[src=\"#{image.url}\"]", 1
+      assert_select "tr[data-image-id=\"#{image.id}\"] .test-tags", text: "Tags: #{image.tag_list}"
+    end
   end
 
   test 'new' do
@@ -24,7 +27,7 @@ class ImagesControllerTest < ActionController::TestCase
 
   test 'create' do
     assert_difference -> { Image.count } do
-      post :create, image: { title: 'some image', url: 'http://someawesomeimage.com' }
+      post :create, image: { title: 'some image', url: 'http://someawesomeimage.com', tag_list: 'create_test' }
     end
     assert_redirected_to image_path(assigns(:image))
   end
@@ -40,11 +43,20 @@ class ImagesControllerTest < ActionController::TestCase
 
   test 'show' do
     image_url = 'http://www.horniman.info/DKNSARC/SD04/IMAGES/D4P1570C.JPG'
-    image = Image.create!(title: 'test3Img', url: image_url)
+    image = Image.create!(title: 'test3Img', url: image_url, tag_list: 'tag1, tag2')
 
     get :show, id: image
     assert_response :success
     assert_select "img[src=\"#{image_url}\"]", 1
     assert_select '#image_title', text: 'test3Img'
+    assert_select '#image_tags', text: 'tag1, tag2'
+  end
+
+  test 'show with empty tags' do
+    image_url = 'http://www.horniman.info/DKNSARC/SD04/IMAGES/D4P1570C.JPG'
+    image1 = Image.create!(title: 'test3Img', url: image_url, tag_list: '')
+    get :show, id: image1
+    assert_response :success
+    assert_select '#image_tags', text: ''
   end
 end
