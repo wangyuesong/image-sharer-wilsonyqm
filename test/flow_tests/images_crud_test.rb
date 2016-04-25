32b6de9752e19f161f12900c78e361cc254688e7
Qiaomu Yao <wilsonyqm@gmail.com>
2016-04-25 16:44:27 -0700

    qy - add delete image controller test and flow test

--- test/flow_tests/images_crud_test.rb
+++ test/flow_tests/images_crud_test.rb
@@ -26,6 +26,42 @@ class ImagesCrudTest < FlowTestCase
     click_on('Save Image')
 
     page.assert_current_path(image_path(Image.last))
-    page.assert_selector("img[src=\"#{valid_url}\"]")
+    page.assert_selector("img[src=\"#{valid_url}\"]", count: 1)
+  end
+
+  test 'delete an image' do
+    image1 = Image.create!(title: 'delete_test_1', url: 'http://www.horniman.info/DKNSARC/SD04/IMAGES/D4P1570C.JPG')
+    image_to_delete = Image.create!(title: 'delete_test_2', url: 'https://upload.wikimedia.org/wikipedia/commons/9/92/Colorful_spring_garden.jpg')
+    visit(root_path)
+    page.assert_selector("img[src=\"#{image_to_delete.url}\"]", count: 1)
+    page.assert_selector("img[src=\"#{image1.url}\"]", count: 1)
+
+    dismiss_confirm('Are you sure') do
+      page.find("div[data-image-id=\"#{image_to_delete.id}\"] a[data-method=\"delete\"]").click
+    end
+    page.assert_selector("img[src=\"#{image_to_delete.url}\"]", count: 1)
+    page.assert_no_text('Image deleted')
+
+    accept_confirm('Are you sure') do
+      page.find("div[data-image-id=\"#{image_to_delete.id}\"] a[data-method=\"delete\"]").click
+    end
+    page.assert_current_path(images_path)
+    page.assert_selector("img[src=\"#{image_to_delete.url}\"]", count: 0)
+    page.assert_selector("img[src=\"#{image1.url}\"]", count: 1)
+    page.assert_selector('#flash_messages', count: 1, text: 'Image deleted' )
+  end
+
+  test 'delete an not exist image' do
+    image_to_delete = Image.create!(title: 'delete_test_2', url: 'https://upload.wikimedia.org/wikipedia/commons/9/92/Colorful_spring_garden.jpg')
+    visit(root_path)
+    page.assert_selector("img[src=\"#{image_to_delete.url}\"]", count: 1)
+    image_to_delete.destroy!
+    page.assert_selector("img[src=\"#{image_to_delete.url}\"]", count: 1)
+    accept_confirm('Are you sure') do
+      page.find("div[data-image-id=\"#{image_to_delete.id}\"] a[data-method=\"delete\"]").click
+    end
+    page.assert_current_path(images_path)
+    page.assert_selector('#flash_messages', text: 'Image does not exist', count: 1 )
+    page.assert_selector("img[src=\"#{image_to_delete.url}\"]", count: 0)
   end
 end
