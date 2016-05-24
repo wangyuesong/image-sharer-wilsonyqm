63fccbd840e2113a853a329533f0a288581f4017
Qiaomu Yao <wilsonyqm@gmail.com>
2016-05-23 17:20:03 -0700

    qy - rollback and migrate to make db/schema.rb accurate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160426212826) do
+ActiveRecord::Schema.define(version: 20160426204118) do
 
   create_table "images", force: :cascade do |t|
     t.string "title"
@@ -45,7 +45,9 @@ ActiveRecord::Schema.define(version: 20160426212826) do
     t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest", null: false
+    t.string "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
