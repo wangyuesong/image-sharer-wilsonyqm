919d2d4ae39f6b2f6242fe101c4f9a578204abff
Yuesong Wang <573978301@qq.com>
2016-08-01 10:26:55 -0700

    tsdfsdfasdest

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,27 +6,27 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user = User.create!(email: 'qiaomu@appfolio.com', name: 'Qiaomu', password: 'password')
-
-image_urls = ['http://www.appfolio.com/images/html/apm-fb-logo.png',
-              'http://www.appfolio.com/images/html/apm-mobile-nav2-logo.png',
-              'http://leaserentmanage.com/wp-content/uploads/2014/09/appfolio.png',
-              'http://www.saimgs.com/imglib/other_pages/case_studies/appfolio%20accounting.png',
-              'http://larsenpropertymanagement.com/wp-content/uploads/2014/11/appfolio-leasing-dashboard.jpg',
-              'https://lh6.ggpht.com/aeJW66gSw0oDrLDAII05Whi7seLbdNMB0HROG9JtUzjKDi8rH6fN2dhuWrtkVTaUrRA=h900',
-              'https://i.ytimg.com/vi/BfW8Az1uoj4/hqdefault.jpg',
-              'http://cdn.fasstatic.com/directory/appfolio/appfolio-property-manager/screenshot~436.png',
-              'http://image.slidesharecdn.com/serviceandsuccessappfoliofinal1-140428153107-phpapp02/95/appfolio-services-experience-6-638.jpg?cb=1398699152',
-              'http://www.appfolio.com/images/screenshots/screening_report_ss.png',
-              'http://www.statusmania.ge/_fr/0/8880548.jpg',
-              'http://weknowyourdreamz.com/images/beautiful/beautiful-03.jpg',
-              'https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg',
-              'http://www.goodlifelongmont.com/wp-content/uploads/2014/06/shutterstock_110345243.jpg',
-              'http://vafoodbanks.org/wp-content/uploads/2012/06/fresh_food.jpg',
-              'https://www.deliverywow.com/order-food-online/images/miami-italian-food-delivery.jpg',
-              'http://i2.cdn.turner.com/cnnnext/dam/assets/150204162810-chinese-food-hairy-crab-super-169.jpg',
-              'http://sites.psu.edu/siowfa15/wp-content/uploads/sites/29639/2015/09/1782_fast_food_hamburger_drink_and_french_fries.jpg',
-              'http://www.rawfoodlife.com/iStock_000009999916Small.jpg',
-              'http://www.splendidtable.org/sites/default/files/styles/lede_image/public/food%20rainbow.jpg?itok=KX25XbIW']
-
-Image.create!(image_urls.map.with_index { |url, i| { title: "image_seed_#{i}", url: url, tag_list: 'tag', user: user } })
+# user = User.create!(email: 'qiaomu@appfolio.com', name: 'Qiaomu', password: 'password')
+#
+# image_urls = ['http://www.appfolio.com/images/html/apm-fb-logo.png',
+#               'http://www.appfolio.com/images/html/apm-mobile-nav2-logo.png',
+#               'http://leaserentmanage.com/wp-content/uploads/2014/09/appfolio.png',
+#               'http://www.saimgs.com/imglib/other_pages/case_studies/appfolio%20accounting.png',
+#               'http://larsenpropertymanagement.com/wp-content/uploads/2014/11/appfolio-leasing-dashboard.jpg',
+#               'https://lh6.ggpht.com/aeJW66gSw0oDrLDAII05Whi7seLbdNMB0HROG9JtUzjKDi8rH6fN2dhuWrtkVTaUrRA=h900',
+#               'https://i.ytimg.com/vi/BfW8Az1uoj4/hqdefault.jpg',
+#               'http://cdn.fasstatic.com/directory/appfolio/appfolio-property-manager/screenshot~436.png',
+#               'http://image.slidesharecdn.com/serviceandsuccessappfoliofinal1-140428153107-phpapp02/95/appfolio-services-experience-6-638.jpg?cb=1398699152',
+#               'http://www.appfolio.com/images/screenshots/screening_report_ss.png',
+#               'http://www.statusmania.ge/_fr/0/8880548.jpg',
+#               'http://weknowyourdreamz.com/images/beautiful/beautiful-03.jpg',
+#               'https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg',
+#               'http://www.goodlifelongmont.com/wp-content/uploads/2014/06/shutterstock_110345243.jpg',
+#               'http://vafoodbanks.org/wp-content/uploads/2012/06/fresh_food.jpg',
+#               'https://www.deliverywow.com/order-food-online/images/miami-italian-food-delivery.jpg',
+#               'http://i2.cdn.turner.com/cnnnext/dam/assets/150204162810-chinese-food-hairy-crab-super-169.jpg',
+#               'http://sites.psu.edu/siowfa15/wp-content/uploads/sites/29639/2015/09/1782_fast_food_hamburger_drink_and_french_fries.jpg',
+#               'http://www.rawfoodlife.com/iStock_000009999916Small.jpg',
+#               'http://www.splendidtable.org/sites/default/files/styles/lede_image/public/food%20rainbow.jpg?itok=KX25XbIW']
+#
+# Image.create!(image_urls.map.with_index { |url, i| { title: "image_seed_#{i}", url: url, tag_list: 'tag', user: user } })
